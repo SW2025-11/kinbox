e1f837922c8fa4f8065b8809ad6c9ed587a53443
Ubuntu <2311140022m@ed.fuk.kindai.ac.jp>
2026-01-17 12:10:37 +0000

    er5

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    post login_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+  end
+
   test "should get index" do
     get favorites_path
     assert_response :success
   end
 end
-
